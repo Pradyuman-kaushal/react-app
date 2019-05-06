c51b3b4cd22026d3f3022b768b333f7100a6bf6b
Pradyuman kaushal <pradum13@gmail.com>
2019-05-06 13:25:23 +0530

    header done

---
+++ Fandom/src/components/Header.js
@@ -0,0 +1,31 @@
+import React,{Component} from 'react';
+import logo from './logo.png';
+import search from './search.png'
+import './Header.css';
+const p={
+  float:'right'
+};
+const q={
+  margin:'15px'
+};
+
+class Header extends Component{
+
+  render(){
+
+    return(
+
+      <div id="top">
+
+      <img src={logo} alt="logo" id="logo"/>
+      <p id="search">
+      <p class="content" style={q}>Character</p>
+      <input type="text" placeholder="Episode or Character or Location"/>
+      <p class="content" style={p} >
+      <img src={search} /></p>
+      </p>
+      </div>
+    );
+  }
+}
+export default Header;
