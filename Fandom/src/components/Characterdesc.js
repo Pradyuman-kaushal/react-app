93a7ea1f63056cf57cd56e143bfb6ebd110aadcf
Pradyuman kaushal <pradum13@gmail.com>
2019-05-11 10:11:35 +0530

    search bar in progress

--- Fandom/src/components/Characterdesc.js
+++ Fandom/src/components/Characterdesc.js
@@ -1,27 +1,39 @@
 import React,{Component} from 'react';
 import {BrowserRouter as Router,Link} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+import Episodelink from './Episodelink';
 class Characterdesc extends Component{
-
+constructor(){
+  super();
+  this.set=this.set.bind(this);
+}
 state={
   res:[],
 };
   fun(){
     const style=this.state.flag?{width:'320%',height:'100%'}:{display:'none'};
+    const pstyle=this.state.pf?{display:'none'}:{fontSize:'20px',padding:'10px'};
+    const p1style=this.state.pf?{display:'none'}:{fontSize:'20px'};
+    const listyle=this.state.pf?{display:'none'}:{};
+    const ols=this.state.pf?{margin:'0px',padding:'0px'}:{};
+    const imgs=this.state.pf?{display:'none'}:{float:"left",margin:'20px'};
     return <div style={style}>
-    <h1><i>Character - {this.props.item[this.props.id-1].name}</i></h1>
-    <hr/><br/>
-    <span><img src={this.props.item[this.props.id-1].image} style={{float:"left",margin:'20px'}} alt="img"/></span>
-    <p style={{fontSize:'20px',padding:'10px'}}><b>Species : </b>{this.props.item[this.props.id-1].species}<br/><br/><br/>
+    <h1 style={listyle}><i>Character - {this.props.item[this.props.id-1].name}</i></h1>
+    <hr style={listyle}/><br style={listyle}/>
+    <span><img src={this.props.item[this.props.id-1].image} style={imgs} alt="img"/></span>
+    <p style={pstyle}><b>Species : </b>{this.props.item[this.props.id-1].species}<br/><br/><br/>
     <b>Status : </b>{this.props.item[this.props.id-1].status}<br/><br/><br/>
     <b>Gender : </b>{this.props.item[this.props.id-1].gender}<br/><br/><br/>
     <b>Origin : </b>{this.props.item[this.props.id-1].origin.name}<br/><br/><br/>
     <b>Last Location : </b>{this.props.item[this.props.id-1].location.name}<br/></p>
-    <p style={{fontSize:'20px'}}><b>Episodes</b></p>
-    <ol><Router>
+    <p style={p1style}><b>Episodes</b></p>
+    <ol style={ols}>
     {this.state.res.map(i=>{
-        return <li><Link>{i}</Link></li>
+        return <Router><li style={listyle}><Link to={"/episode"+i.name} onClick={this.set}>{i.name}</Link></li>
+        <Route exact path={"/episode"+i.name} render={()=>{return(<Episodelink item={i}/>);}}/>
+          </Router>
     })
-  }</Router>
+  }
     </ol>
       </div>
   }
@@ -29,7 +41,10 @@ state={
           return <li><a href={p}>{console.log(this.state.res)}</a></li>
       }
 
-
+      set(){
+        this.setState({pf:true});
+        this.fun();
+      }
 
 async componentDidMount(){
   var arr=[];
@@ -37,7 +52,7 @@ async componentDidMount(){
   const res=await fetch(this.props.item[this.props.id-1].episode[i]);
   const data=await res.json();
 
-  arr[i]=data.name;
+  arr[i]=data;
 }
   this.setState({res:arr,flag:true});
 }
--- Fandom/src/components/Header.js
+++ Fandom/src/components/Header.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import logo from './logo.png';
 import search from './search.png'
 import './Header.css';
+import Characterlink from './Characterlink';
 const p={
   float:'right'
 };
@@ -10,7 +11,25 @@ const q={
 };
 
 class Header extends Component{
+constructor(){
+  super();
+  this.func=this.func.bind(this);
+  this.state={
+    items:[],
+    ch:""
+  }
 
+}
+funi(){
+  console.log(this.ref.name.value);
+  /*for(var i=0;i<this.state.items.length;i++){
+    if(this.state.items[i].name.equals(this.ref.name.value)){
+
+      return <Characterlink item={this.state.items[i]}/>*/
+
+
+
+}
   render(){
 
     return(
@@ -20,12 +39,26 @@ class Header extends Component{
       <img src={logo} alt="logo" id="logo"/>
       <p id="search">
       <p class="content" style={q}>Character</p>
-      <input type="text" placeholder="Episode or Character or Location"/>
+      <input type="text" placeholder="Enter Any Character" id="ch"/>
       <p class="content" style={p} >
-      <img src={search} alt="search"/></p>
+      <img src={search} alt="search" onClick={this.func}/>
+      </p>
       </p>
       </div>
     );
+  }
+  func(){
+    var x=document.getElementById("ch").value;
+    this.setState({ch:x});
+  }
+  getcha(){
+    return this.state.ch;
+  }
+  async componentDidMount(){
+    const res=await fetch("https://rickandmortyapi.com/api/character/");
+    const data=await res.json();
+    console.log(data);
+    this.setState({items:data.results});
   }
 }
 export default Header;
